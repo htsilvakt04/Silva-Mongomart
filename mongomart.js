abb72a497db58afd8e858ef64e46d10c21febabb
silva <htsilvakt04@gmail.com>
2018-04-05 00:20:49 +0700

    remove monitor app

--- mongomart.js
+++ mongomart.js
@@ -13,7 +13,6 @@
   See the License for the specific language governing permissions and
   limitations under the License.
 */
-require('newrelic');
 require('dotenv').config();
 var express = require('express'),
     bodyParser = require('body-parser'),
