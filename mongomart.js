f563e901e22054658b078edb6c67154514d6e2ff
silva <htsilvakt04@gmail.com>
2018-04-04 20:22:51 +0700

    modify vars

--- mongomart.js
+++ mongomart.js
@@ -307,7 +307,7 @@ MongoClient.connect(url, function(err, db) {
     app.use('/', router);
 
     // Start the server listening
-    var server = app.listen(3000, function() {
+    var server = app.listen(process.env.port, function() {
         var port = server.address().port;
         console.log('Mongomart server listening on port %s.', port);
     });
