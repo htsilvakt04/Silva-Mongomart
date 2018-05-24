1deb632aee5cc97c3f43ad5a21d16a4bf7ff6a46
silva <htsilvakt04@gmail.com>
2018-05-24 12:26:31 +0700

    finish byID reducer (test )

--- react-redux/reducers/byID.js
+++ react-redux/reducers/byID.js
@@ -1,8 +1,27 @@
+function hydrateData(review) {
+    return {
+        comment: review.review,
+        name: review.name,
+        stars: review.stars,
+        date: review.date
+    }
+}
 const byID = (state = {}, action) => {
     switch (action.type) {
         case 'ADD_REVIEW':
-
+            const reviewToAdd = hydrateData(action.review);
+            const id = action.review.id;
+            return {
+                [id]: {
+                    ...state[id],
+                    reviews: [
+                        ...state[id].reviews,
+                        reviewToAdd
+                    ]
+                }
+            }
         default:
             return state;
     }
 }
+export default byID;
